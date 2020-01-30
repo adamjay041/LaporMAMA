d90b59ea67a309033640ade4073f9db30d7d3d8d
andrumahardi <andrumahardi77@gmail.com>
2020-01-31 02:46:31 +0700

    login

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,14 @@ const router = express
 const Lesson = require('./leson')
 const Student = require('./appRouter')
 const Score = require('./addScore')
+
 router.use(Student)
 router.use(Lesson)
 router.use(Score)
+
+router.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
 
 
 module.exports = router
