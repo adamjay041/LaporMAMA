04d41999fc16ea655ad38b0151ef27d96333cc47
andrumahardi <andrumahardi77@gmail.com>
2020-01-31 02:36:06 +0700

    half done

--- migrations/20200129122315-create-student.js
+++ migrations/20200129122315-create-student.js
@@ -14,9 +14,6 @@ module.exports = {
       totalScore: {
         type: Sequelize.INTEGER
       },
-      totalAbsen: {
-        type: Sequelize.INTEGER
-      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
